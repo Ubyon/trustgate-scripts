08fe9a8377a77819a5318a8226cbc7b9968848bb
Hexin Wang <hexin.wang@ubyon.com>
2023-09-21 14:20:53 -0700

    Used etimes that outputs elapsed time in seconds in integer.

--- guacd_cleanup.sh
+++ guacd_cleanup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# This script kills processes that runs longer than 12 hours in the system.
+# This script kills guacd processes that runs longer than 12 hours in the system.
 
 set -e
 set -x
@@ -8,15 +8,8 @@ set -x
 # Define the threshold time in seconds (12 hours)
 threshold=$((12 * 60 * 60))
 
-# Get the current timestamp in seconds
-# current_time=$(date +%s)
-
 # Iterate through all processes
-while read -r pid elpased_time; do
-  if [ "$pid" == "1" ]; then
-    continue
-  fi
-
+while read -r pid elapsed_time comm; do
   # Calculate the age of the process in seconds
   process_age=$((elapsed_time))
 
@@ -26,6 +19,6 @@ while read -r pid elpased_time; do
     echo "Killing process $pid (started $process_age seconds ago)"
     kill "$pid"
   fi
-done < <(ps -eo pid=,etime=)
+done < <(ps -eo pid,etimes,command |grep guacd |grep -v grep |tail -n +2)
 
 echo "Process cleanup completed."
